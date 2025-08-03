ff42bed996ad14e933a0a0aef8504e2624f014ed
nirmal428 <mauryanirmal428@gmail.com>
2025-08-03 23:33:47 +0530

    init commit

---
+++ src/Array/input_array.java
@@ -0,0 +1,20 @@
+package Array;
+
+import java.util.Scanner;
+
+public class input_array {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter size of array : ");
+        int n =sc.nextInt();
+        System.out.print("enter array element : ");
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
