72149337c203a76b610f46b5ebeb2e6975f69c55
nirmal428 <mauryanirmal428@gmail.com>
2025-10-05 12:52:29 +0530

    init commit

--- src/DivideAndConquer/backtracking/N_Queens.java
+++ src/DivideAndConquer/backtracking/N_Queens.java
@@ -44,7 +44,7 @@ public class N_Queens {
         System.out.println("_____chess board______");
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board.length; j++){
-                System.out.print(board[i][j]);
+                System.out.print(board[i][j] +" ");
             }
             System.out.println();
         }
