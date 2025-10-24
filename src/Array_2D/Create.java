61299e5a062224656b35df0d9e340621fa1ec7eb
nirmal428 <mauryanirmal428@gmail.com>
2025-10-24 21:45:57 +0530

    init commit

---
+++ src/Array_2D/Create.java
@@ -0,0 +1,39 @@
+package Array_2D;
+
+import java.util.Scanner;
+
+public class Create {
+
+    public static boolean search(int matrix[][],  int key){
+        for(int i=0; i< matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j]==key){
+                    System.out.print("found at cel("+i+","+j+")");
+                    return true ;
+                }
+            }
+        }
+        System.out.println("key not found ");
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][]=new int[3][3];
+        int n=matrix.length;
+        int m=matrix[0].length;
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        search(matrix,5);
+    }
+}
