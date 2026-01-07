17bc9a0494c8d2899313692aa2bc13bde0595299
nirmal428 <mauryanirmal428@gmail.com>
2026-01-07 23:58:40 +0530

    init commit

---
+++ src/Condition/largest.java
@@ -0,0 +1,16 @@
+package Condition;
+
+import com.sun.security.jgss.GSSUtil;
+
+public class largest {
+    public static void main(String[] args) {
+        int a=15;
+        int b=20;
+        if(a>=b){
+            System.out.println("largest = "+a);
+        }
+        else{
+            System.out.println("largest = "+b);
+        }
+    }
+}
