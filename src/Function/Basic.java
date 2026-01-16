693799b33e1229bb4ec3cc175e358d625367ecfd
nirmal428 <mauryanirmal428@gmail.com>
2026-01-17 01:05:39 +0530

    init commit

---
+++ src/Function/Basic.java
@@ -0,0 +1,11 @@
+package Function;
+
+public class Basic {
+
+    public static void printHello(){
+        System.out.println("HelloWord");
+    }
+    public static void main(String[] args) {
+        printHello();
+    }
+}
