cc3ac38d9d23878cbf83eb14c90337ef4101ac8d
nirmal428 <mauryanirmal428@gmail.com>
2026-01-19 19:01:54 +0530

    init commit

--- src/Array/marksOfStudent.java
+++ src/Array/marksOfStudent.java
@@ -1,20 +1,20 @@
-package Pattern;
+package Array;
 
 import java.util.Scanner;
 
 public class marksOfStudent {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter number of student ");
-        int n =sc.nextInt();
-        System.out.print("Enter student mark : ");
+        System.out.print("Enter number of student : ");
+        int n=sc.nextInt();
+        System.out.print("Enter Mark or Student :");
         int[] arr=new int[n];
         for(int i=0;i<=n-1;i++){
-            arr[i] = sc.nextInt();
+            arr[i]=sc.nextInt();
         }
-        for(int i=0;i<=n-1;i++){
+        for(int i=0;i<n;i++){
             if(arr[i]<35){
-                System.out.println(i+" ");
+                System.out.print(i+" ");
             }
         }
     }
