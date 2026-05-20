8ccd17e69ffd1b5ba9034a222242eb118659a5a0
Nirmal Maurya <mauryanirmal428@gmail.com>
2026-05-20 22:26:27 +0530

    init commit

---
+++ src/Array/reverse.java
@@ -0,0 +1,19 @@
+package Array;
+
+import java.util.Scanner;
+
+public class reverse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n =sc.nextInt();
+        System.out.print("Target: ");
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+
+        }
+    }
+}
